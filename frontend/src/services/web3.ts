b51bed5c870d34d6300e1865d19bf622d77ed9fa
Felipe Saadi <54749257+FelipeSaadi@users.noreply.github.com>
2023-09-18 11:53:53 -0300

    feat: add metamsk

--- frontend/src/services/web3.ts
+++ frontend/src/services/web3.ts
@@ -1,7 +1,21 @@
+import { MetaMaskSDK, MetaMaskSDKOptions } from "@metamask/sdk";
+
+const options: MetaMaskSDKOptions = {
+  injectProvider: false,
+  // checkInstallationImmediately: true,
+  dappMetadata: { name: "7vision", url: "https://7vfi.ai" },
+  preferDesktop: true,
+};
+
+const MMSDK = new MetaMaskSDK(options);
+// ethereum.isConnected
 const web3Service = {
   connectMetamask: async () => {
-    alert('connected')
-  }
-}
+    const ethereum = MMSDK.getProvider();
 
-export default web3Service
+    // console.log(ethereum);
+    await ethereum.request({ method: "eth_requestAccounts", params: [] });
+  },
+};
+
+export default web3Service;
